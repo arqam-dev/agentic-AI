d5eb7727b01a71998b7fff8a65dd0c57cabf58f8
Aqsa Tanoli <116660401+aqsatanoli@users.noreply.github.com>
2025-05-17 20:56:12 +0800

    Update app.py

---
+++ spam detector agent/app.py
@@ -0,0 +1,25 @@
+from sklearn import flask,josify , request
+from sklearn.feature_extraction.text  import CountVectorizer, TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+import re
+
+app = (__name__)
+
+texts =[
+
+#spam text
+
+
+
+
+
+#ham text
+
+
+
+
+
+
+
+
+]
